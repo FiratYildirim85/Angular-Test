c2afd8b26b1572dedefa9641a3a46ef1b74b1d78
FiratYildirim85 <firat.yildirim2904@gmail.com>
2022-11-30 13:15:39 +0100

    added images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Angular-Test';
+export class AppComponent implements OnInit {
+  images = ['coding.jpg', 'computer.jpg', 'software.jpg'];
+  currentImage = 0
+
+  ngOnInit() {
+    this.updateImage();
+
+  }
+  updateImage() {
+    setInterval(()=>{
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+    },8000)
+
+  }
 }
